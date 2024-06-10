30f1ecfa13a3fe88c6bb2925ff18a38cef9910b2
Vicente Pinochet <vicente.pinochet.r@ug.uchile.cl>
2024-06-10 12:40:05 -0400

    Create hola_mundo.py

---
+++ hola_mundo.py
@@ -0,0 +1,13 @@
+"""
+hola_mundo.py
+
+Una aplicación simple de "Hola, Mundo!" siguiendo la guía de estilo PEP 8.
+"""
+
+def main():
+    """Función principal para imprimir 'Hola, Mundo!'."""
+    print("Hola, Mundo!")
+
+
+if __name__ == "__main__":
+    main()
